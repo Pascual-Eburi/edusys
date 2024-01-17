253df00d0df89cde3707d0af899927c7df5f235e
Pascual Eburi <pascualeburi@gmail.com>
2024-01-17 09:31:30 +0100

    Defined realationship with assessment period dates table

--- v1.1/app/Models/AcademicCourse.php
+++ v1.1/app/Models/AcademicCourse.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AcademicCourse extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'start_date', 'end_date'];
+
+    public function assessmentPeriodDates(): HasMany
+    {
+        return $this->hasMany(AssessmentPeriodDates::class);
+    }
 }
