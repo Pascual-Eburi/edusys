3c6934fbeb32d3e45b7d039ed2a60c16b70548cf
Pascual Eburi <pascualeburi@gmail.com>
2024-01-22 10:40:47 +0100

    wip: AssessmentPeriodDatesFactory

---
+++ v1.1/database/factories/AssessmentPeriodDatesFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AssessmentPeriodDates>
+ */
+class AssessmentPeriodDatesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
