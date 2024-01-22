0c79423600e5f7279c65bfbe7d8ad7b4e2d1a2d5
Pascual Eburi <pascualeburi@gmail.com>
2024-01-22 10:40:36 +0100

    wip: DatabaseSeeder

--- v1.1/database/seeders/DatabaseSeeder.php
+++ v1.1/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        
+        $this->call([
+            CourseSeeder::class,
+            SectionSeeder::class,
+            AcademicCourseSeeder::class,
+            EducationLevelSeeder::class,
+            AssessmentPeriodSeeder::class,
+            
+            RoleSeeder::class,
+            UserSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
